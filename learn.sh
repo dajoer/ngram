28528bcab22fa4a498d30e14f38c20f14ab901a6
gnvnix <davidjoeressen@googlemail.com>
2016-02-09 13:52:37 +0100

    Updated learn.sh

--- learn.sh
+++ learn.sh
@@ -1,2 +1,6 @@
-#!/bin/zsh
-grep -h '^:' data/* | sentsplit | ngram --learn langModel.json
+#!/bin/bash
+if [ -z $1 ]; then
+	echo "Usage: ./learn.sh <filename>"
+else
+	grep -h '^:' data/* | sentsplit | ngram --learn $1
+fi
