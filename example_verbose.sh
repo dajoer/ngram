181a4b270f83104f902f30162c3c7bd77f8efe95
dajoer <s2dajoer@uni-trier.de>
2016-02-19 23:03:48 +0100

    Edit example

--- example_verbose.sh
+++ example_verbose.sh
@@ -1,2 +1,6 @@
-#!/bin/zsh
-heapsAlg our point of second meeting | ngram -v langModel.json | sort -gr | less
+#!/bin/bash
+if [ -z $1 ]; then
+	echo "Usage: ./example_verbose.sh <filename>"
+else
+	heapsAlg our point of second meeting | ngram -v $1 | sort -gr | less
+fi
